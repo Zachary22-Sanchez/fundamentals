3342cc4815429977fc375a6fd3e7cca10b35d2b0
Zachary Sanchez <Zacharyesanchez22@gmail.com>
2019-09-27 11:20:28 -0600

    fundementals of variables and data

--- variables-and-data.js
+++ variables-and-data.js
@@ -11,11 +11,20 @@ let totalWithTax = tax + subTotal;
 
 let obj = {
 	key1: "value 1",
-	key2: 3,
+	key2: 4,
+	boolean: true,
 	obj2: {
-		objkey1 : "value of inner object",
+		objkey1 : "Internal Object Value",
 	},
 };
+
+let obj = {
+	key1:"Value 1",
+	key2: 3,
+	obj2: {
+		objKey1 : "value of inner object"
+	}
+}
 
 console.log(obj.obj2.obj2Key1);
 
@@ -127,4 +136,31 @@ while (x < 10) {
 }
 
 
+/*
+ * Objects, Interfaces, and API's
+ */
+
+let dog = {
+	name: "Steve",
+	color: "brown",
+	breed: "Pug",
+	size: "Small",
+	bark: function(typeOfBark){
+		console.log("Bark!");
+	}
+};
+
+dog.bark();
+
+function x(y) {
+	y.num = y.num + 5;
+	console.log(y);
+}
+
+let y = {
+	name: "Tom",
+	num: 10,
+};
+x(y);
+console.log(y);
 
